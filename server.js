edeae417cfe8a3a30eec046f6c6de9dd61968840
T Schway <tony.schwebach@gmail.com>
2021-01-14 13:33:07 -0500

    created express server

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 //REQUIRE EXPRESS
+const express = require("express");
+const path = require("path");
 
 //CREATE INSTANCE OF EXPRESS
+const app = express();
 
 //CREATE PORT FOR THE APP TO RUN
 //BUT ALSO LISTEN TO ENVIRONMENT VARIABLES SO WE CAN DEPLOY TO HEROKU
+const PORT = process.env.PORT || 3000;
 
 //ADD MIDDLEWARE
 //COPY/PASTE FROM STAR WARS APP
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //CREATE AN ARRAY OF TABLES
+const tables = [];
 //CREAT AN ARRAY FOR WAITLIST
+const waitlist = [];
 
 //ARRAY OF OBJECTS. OBJECTS SHOULD HAVE
 //NAME
@@ -27,4 +35,6 @@
 //CREATE TABLE/RESERVATION
 
 //LISTEN ON THE PORT
-
+app.listen(PORT, function () {
+  console.log(`app listening on http://localhost:${PORT}`);
+});
