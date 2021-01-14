86792c5356e238ac209c9b66db461ff07cf343ee
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-14 13:07:21 -0500

    pseudocode

---
+++ server.js
@@ -0,0 +1,22 @@
+//dependencies
+//1. express, 2.path
+
+//Setup
+//setup express app
+//setup express app to handle data parsing
+
+//Data
+//waiting list, table array
+
+// Routes
+// basic route
+// array of tables
+// waiting list
+// get methods
+// post method (waiting list and table)
+// /api :table or :waitlist
+
+//start server to listen
+
+
+
